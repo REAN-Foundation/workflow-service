04629777cf011d6be5149d469da7670fff7e7e31
devendra-rean <devendra.suthar@reanfoundation.org>
2026-06-16 18:25:13 +0530

    feat: update entrypoint.sh for error handling and add .dockerignore file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+set -e
+
 # Add config/creds copying here..
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/env.config /app/.env
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/internal.clients.seed.json /app/seed.data/internal.clients.seed.json
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/default.users.seed.json /app/seed.data/default.users.seed.json
+if ! aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/env.config /app/.env; then
+	echo "Failed to copy env.config from S3."
+	exit 1
+fi
+
+if ! aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/internal.clients.seed.json /app/seed.data/internal.clients.seed.json; then
+	echo "Failed to copy internal.clients.seed.json from S3."
+	exit 1
+fi
+
+if ! aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/default.users.seed.json /app/seed.data/default.users.seed.json; then
+	echo "Failed to copy default.users.seed.json from S3."
+	exit 1
+fi
 
 cd /app
 # Add any other scripts here...
